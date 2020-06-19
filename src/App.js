18dbb77419f775e4f697d518e3d4d2aaef705cb2
Ashley20 <kucukkaraaslan.didem@gmail.com>
2020-06-19 14:32:22 +0300

    create fetchData method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from 'react';
 import { Cards, Chart, CountryPicker} from "./components";
 import "./app.module.css";
+import { fetchData } from "./api";
 
 class App extends Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data)
+  }
+
   render() {
     return (
       <div className="container">
